040d443d54ffc4782fa8fd4f73fd9570dd0ac004
Will <1346508197@qq.com>
2019-01-25 19:57:11 +0800

    尝试多行注释

--- 1.py
+++ 1.py
@@ -1,19 +1,24 @@
-import tkinter
-from tkinter import messagebox
+def fun1(x, y):
+    '''Hello World'''  # 注释放在第一行
+    x[:] = y
+    x = [0, 0]
+    # 此时形参x是实参x的一个副本，存储着实参x的指针，
+    # 修改形参x时是对实参的地址进行操作，而对形参x赋值时，是用另一个指针覆盖了它对实参的引用
+
+x = [1,2]
+y = [3,4]
+
+fun1(x, y)
+print(x, y)
+print(fun1.__doc__)
+fun1.__doc__="Hi"
+print(fun1.__doc__)
+
+# b='''123#'Hello'
+#     + "world"!!! '''
+
+# print(b)
+# print(b.encode())
 
 
-tk_main = tkinter.Tk()
-
-tk_main.attributes("-topmost", True,     # 使该窗口置顶
-                   #"-fullscreen", True,  # 使该窗口为全屏
-                   #"-disabled", True,    # 使该窗口无法操作（用户对它的点击、输入统统无效）
-                   #"-toolwindow", True,  # 使该窗口为toolwindow
-                   #"-alpha", 0.9,        # 设置该窗口的透明度，0代表完全透明，1代表完全不透明
-                   )
-
-
-img = tkinter.PhotoImage(file=r"C:\Users\Will\Desktop\scene.png", width=200, height=100)
-
-
-tk_main.mainloop()
-
+#试试变长参数
