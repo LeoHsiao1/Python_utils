bded919757dfc85a2f4cdb2874fa1868fa7dfce1
Will <1346508197@qq.com>
2019-01-29 18:32:36 +0800

    练习类的专有方法

--- 1.py
+++ 1.py
@@ -1,15 +1,43 @@
-def fun1(x, y):
-    x[:] = y
-    x = [0, 0]
-    # 形参是实参的一个副本，两者都是指向某个地址的指针
-    # 修改形参时是对实参所指的地址进行操作，而对形参赋值时，是用另一个指针覆盖了它
+class Test:
+    a = 0
+    b = 0
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        print("initial completed.")
 
-x = [1,2]
-y = [3,4]
+    def fun1(self, a=a, b=b):  # 这里表示如果调用方法fun1()时没有传入实参a、b，就默认使用类变量a、b的值
+        print(a, b)
+        print(self.a, self.b)  # 在函数内的作用域不能直接访问外部的类变量，要通过self指针获得类变量的引用
 
-fun1(x, y)
-print(x, y)
+    def fun2(self):
+        pass
+
+
+t1 = Test(a=1, b=2)
+
+# t1.fun1()
+
+class Test:
+    def __str__(self):
+        return "__str__"
+
+    def __repr__(self):
+        return "__repr__"
+
+    def __sub__(self, other):
+        return 0 - other
+
+    def __rsub__(self, other):
+        return other - 0
+
+    def __len__(self):
+        return 0
+
+# t1 = Test2()
+# t1
+# str(t1)
+# print(t1)
 
 
 
-#试试变长参数
