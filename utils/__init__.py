68764c15d12ce598bbf5741fb060c2efb3b21056
Will <1346508197@qq.com>
2019-04-15 15:09:46 +0800

    补充了__all__

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,10 @@
 """
 一个自定义的包，包含一些可通用的模块。
-  - 可以将该包拷贝到其它项目中使用，但保持test_Python项目中的utils包为最新，其它地方沿用老版本。
+  - 可以将该包拷贝到其它项目中使用，也可以作为git的submodule被其它项目引用。
+  - 保持 https://github.com/LeoHsiao1/test_Python.git 处的utils包为最新版本。
 """
+
+__all__ = ["use_io", "use_os", "use_time", "use_form", "use_log", "use_plot"]
+
+for i in __all__:
+    from . import i
