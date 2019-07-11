a0df0c275b629745e019708b40351bee7947faa8
Will <1346508197@qq.com>
2019-07-11 17:11:08 +0800

    试用Flask

--- 1.py
+++ 1.py
@@ -1,12 +1,5 @@
-import hashlib
+import os
+import requests
 
-h = hashlib.sha256()    # 生成一个hash器
-h.update("Hello".encode())      # 输入要hash的内容（必须转换成bytes类型）
-h.update("World".encode())
-h.digest()              # 获取hash结果（也是bytes类型）
 
-# 简化成一步
-hashlib.sha256(b"Hello World").digest()
-
-# 通过对原始口令加一个复杂字符串来实现，俗成“加盐
-# 加盐、加用户名
+os.path.relpath("D:\\1", "D:\\")
